e888fd18370319e6a22f13d1b2f9195af6a0d832
Mathi Fonseca <mathifonseca@gmail.com>
2015-04-24 11:56:29 -0300

    deleted unused URL mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,11 +2,7 @@ class UrlMappings {
 	static mappings = {
 		"/$controller/$action?/$id?"{
 			constraints {
-				// apply constraints here
 			}
 		}
-
-		"/"(view:"/index")
-		"500"(view:'/error')
 	}
 }
